adf0db9c1daf8de951cc7cec7e1625f2dd918418
root <root@DESKTOP-9CK6JKD.localdomain>
2025-06-05 04:35:51 +0000

    echo

--- app/main.go
+++ app/main.go
@@ -10,18 +10,6 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
-// func main() {
-// 	// Uncomment this block to pass the first stage
-// 	fmt.Fprint(os.Stdout, "$ ")
-
-// 	// Wait for user input
-// 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Fprintf(os.Stdout, "%s: command not found", command[:len(command)-1])
-// }
-
 func main() {
 	// Print the shell prompt
 	for {
@@ -38,6 +26,16 @@ func main() {
 		words := strings.Fields(command)
 		if words[0] == "exit" {
 			return
+		}
+
+		if words[0] == "echo" {
+			// Print the rest of the command
+			if len(words) > 1 {
+				fmt.Fprintln(os.Stdout, strings.Join(words[1:], " "))
+			} else {
+				fmt.Fprintln(os.Stdout, "")
+			}
+			continue
 		}
 		fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimSpace(command))
 	}
